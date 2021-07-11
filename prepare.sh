58d2a7761c846ae17f46acae9ea63db6065a6848
rocrover <is.binchen@gmail.com>
2021-07-10 20:06:26 -0700

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo apt-get update
-pip3 install Pillow
-sudo apt-get -y install tesseract-ocr
+sudo apt-get -y install python3 python3-pip tesseract-ocr=4.1.1-2build2
+#apt-cache policy  tesseract-ocr
 pip3 install -r requirements.txt
